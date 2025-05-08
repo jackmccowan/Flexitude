31a35e5fe8dad74c74f5dac742cbf29d22c5b0e2
Michael White <mw250604@gmail.com>
2025-05-09 00:17:41 +1000

    welcome message

--- Flexitude/Modules/Home/Views/HomeView.swift
+++ Flexitude/Modules/Home/Views/HomeView.swift
@@ -8,20 +8,29 @@
 import SwiftUI
 
 struct HomeView: View {
+    @ObservedObject var viewModel: AuthViewModel
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Home")
-                    .font(.largeTitle)
-                    .padding()
+            VStack(alignment: .leading, spacing: 20) {
+                if let firstName = viewModel.currentUser?.firstName {
+                    Text("Welcome back, \(firstName)!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                } else {
+                    Text("Welcome!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
                 
                 Spacer()
             }
+            .padding()
             .navigationTitle("Home")
         }
     }
 }
 
 #Preview {
-    HomeView()
+    HomeView(viewModel: AuthViewModel())
 } 
